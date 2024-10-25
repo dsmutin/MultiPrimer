3ddf659fd210c66832e1633fe4d65af1de8b16e7
dsmutin <98605724+dsmutin@users.noreply.github.com>
2024-10-25 20:50:58 +0300

    add gzipping

--- exec.sh
+++ exec.sh
@@ -3,6 +3,10 @@ prepare () {
     rm test/contigs
     rm -r test/blastn_base
     rm -r test/output
+
+    gzip -d test/blastn_base/true_base/*
+    gzip -d test/fasta_base/false_base_1/*
+    gzip -d test/fasta_base/false_base_2/*
     
     bash prep_db.sh \
     -n test/blastn_base/true_base \
